b8a1f2279c4ea70f2de0058b517a1350f020ff0f
madams23 <mark.adams@ed.ac.uk>
2020-07-27 15:44:04 +0100

    FinnGen: number chr X as 23

--- scripts/sumstats/FinnGen.sh
+++ scripts/sumstats/FinnGen.sh
@@ -7,6 +7,7 @@ daner=$(dirname $daner_gz)/$(basename $daner_gz .gz)
 
 echo -e "CHR\tSNP\tBP\tA1\tA2\tFRQ_A_23424\tFRQ_U_192220\tINFO\tOR\tSE\tP" > $daner
 
-zcat $text_gz | tail -n +2 | awk -v OFS='\t' '{print $1, $5, $2, $4, $3, $11, $12, $18, exp($8), $9, $7}' >> $daner
+# convert chromsome X to 23
+zcat $text_gz | tail -n +2 | awk -v OFS='\t' '{if($1 == "X") {print 23, $5, $2, $4, $3, $11, $12, $18, exp($8), $9, $7} else {print $1, $5, $2, $4, $3, $11, $12, $18, exp($8), $9, $7}}' >> $daner
 
 gzip --verbose $daner
