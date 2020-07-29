b2e578631b6419259ea8d29f3cf33111b3a6a2b6
Mark James Adams <mark.adams@ed.ac.uk>
2020-07-29 12:30:09 +0200

    Handle non-autosomes during liftover
    Assumes daner numbered chromosomes

--- scripts/liftover.R
+++ scripts/liftover.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(readr)
 library(rtracklayer)
 
+
 # input daner in hgFROM
 daner_hg_from_gz <- snakemake@input$daner
 daner_hg_from <- read_table2(daner_hg_from_gz)
@@ -16,11 +17,19 @@ daner_hg_to_gz <- snakemake@output[[1]]
 hgToHg <- import.chain(chain_path)
 
 # create genomic ranges for sumstats
-sumstats_gr_hg_from <- GRanges(
-	seqnames=paste0('chr', daner_hg_from$CHR),
-	ranges=IRanges(daner_hg_from$BP, width=1),
-	SNP=daner_hg_from$SNP
-)
+# format chromosomes as 'chrN'
+# convert non-autosome numeric chromosomes to text
+sumstats_gr_hg_from <- 
+with(daner_hg_from,
+GRanges(
+	seqnames=paste0('chr', case_when(CHR %in% 1:22 ~ as.character(CHR),
+                                   CHR == 23 ~ 'X',
+                                   CHR == 24 ~ 'Y',
+                                   CHR == 25 ~ 'XY',
+                                   CHR == 26 ~ 'MT')),
+	ranges=IRanges(BP, width=1),
+	SNP=SNP
+))
 	
 # liftover from hgFROM to hgTO
 sumstats_gr_hg_to <- liftOver(sumstats_gr_hg_from, hgToHg)
@@ -29,6 +38,6 @@ sumstats_gr_hg_to <- liftOver(sumstats_gr_hg_from, hgToHg)
 daner_hg_to <-
 daner_hg_from %>%
 inner_join(as.data.frame(sumstats_gr_hg_to), by='SNP') %>%
-select(CHR, SNP, BP=start, A1, A2, starts_with('FRQ'), FROMFO, OR, SE, P)
+select(CHR, SNP, BP=start, A1, A2, starts_with('FRQ'), INFO, OR, SE, P)
 
 write_tsv(daner_hg_to, path=daner_hg_to_gz)
