d5a392b28c3829be4bb99164a2c2777a7f9c80d8
Mark James Adams <mark.adams@ed.ac.uk>
2020-09-22 15:53:39 +0100

    Cohorts case/control count table

---
+++ scripts/reports/cohorts_table.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# open case/control count spreadsheet
+casecontrol= pd.read_excel(snakemake.input[0])
+
+# clean up cohorts filename text
+casecontrol['Dataset'] = casecontrol['Dataset'].map(lambda dataset: dataset.replace('.aligned.gz', ""))
+
+# write out as a plain table
+with open(snakemake.output[0], 'w') as out:
+    print(casecontrol, file=out)
