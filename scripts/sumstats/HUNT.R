014cb7f358f761be59c7f250a1177ea5dc7f7ac6
Mark James Adams <mark.adams@ed.ac.uk>
2020-10-17 17:29:42 +0200

    Meta: Process HUNT sumstats info into pre-allocated matrix

--- scripts/sumstats/HUNT.R
+++ scripts/sumstats/HUNT.R
@@ -9,7 +9,8 @@ text_gz=args[1]
 daner_gz=args[2]
 logfile=args[3]
 
-sumstats <- read_table2(text_gz, comment='##')
+cat(paste('Opening', text_gz, '\n'))
+sumstats <- read_table2(text_gz, comment='##', col_types='cicccccc')
 
 # data that needs to be converted to numeric
 sumstats_nums <- c("BETA", "SE", "PVAL",
@@ -28,15 +29,29 @@ assign_key_values <- function(INFO) {
 }
 
 # parse key/values and bind into a data.frame
-sumstats_info <- bind_rows(lapply(sumstats$INFO, assign_key_values))
+cat(paste('Parsing INFO', '\n'))
+# this could be done functionally but that requries a lot of intermediate
+# memory usage
+# sumstats_info <- bind_rows(lapply(sumstats$INFO, assign_key_values))
+# pre-allocate the final matrix and use a loop instead
+
+sumstats_info <- matrix(NA, nrow=nrow(sumstats), ncol=length(sumstats_nums), dimnames=list(NULL, sumstats_nums))
+
+M <- nrow(sumstats)
+for(i in seq.int(M)) {
+	sumstats_info[i,] <- as.numeric(assign_key_values(sumstats$INFO[i])[sumstats_nums])
+	if(i %% 100000 == 1) {
+		cat(paste0(i, '/', M, '\n'))
+	}
+}
 
 # merge sumstats with the SNP columns
 # convert stats columns to numeric
+cat(paste('Merging SNP info', '\n'))
 sumstats_stats <-
 sumstats %>%
 select(-QUAL, -FILTER, -INFO) %>%
-bind_cols(sumstats_info) %>%
-mutate_at(sumstats_nums, as.numeric)
+bind_cols(sumstats_info)
 
 rm(sumstats); rm(sumstats_info); gc()
 
@@ -46,12 +61,15 @@ Ncontrols <- max(sumstats_stats$NCON)
 FRQ_A_col <- paste0('FRQ_A_', Ncases)
 FRQ_U_col <- paste0('FRQ_U_', Ncontrols)
 
+cat(paste('Formatting daner', '\n'))
 daner <-
 sumstats_stats %>%
-mutate(OR=exp(BETA)) %>%
+mutate(CHR=if_else(`#CHROM` == 'X', true=23, false=as.numeric(`#CHROM`)),
+       OR=exp(BETA)) %>%
 filter(between(FCAS, 0.01, 0.99) & between(FCON, 0.01, 0.99)) %>%
-select(CHR=`#CHROM`, SNP=ID, BP=POS, A1=ALT, A2=REF,
+select(CHR, SNP=ID, BP=POS, A1=ALT, A2=REF,
   	   !!FRQ_A_col:=FCAS, !!FRQ_U_col:=FCON, INFO=R2,
 	   OR, SE, P=PVAL, ngt=NGT)
-	   
+
+cat(paste('Writing daner', '\n'))   
 write_tsv(daner, daner_gz)
