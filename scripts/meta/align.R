ab0a5780fa527a4179503606fe8f3c7e62f44e4c
Mark James Adams <mark.adams@ed.ac.uk>
2020-10-19 15:57:55 +0200

    Meta: filter allele frequencies during alignment. Reorder columns and rows

--- scripts/meta/align.R
+++ scripts/meta/align.R
@@ -32,19 +32,23 @@ lapply(impute_frq2_files,
                                     FA1 = col_double(),
                                     NCHROBS = col_integer()
 )))) %>%
-select(-FA1, -NCHROBS)
+select(-NCHROBS)
 
 gc()
 
 # merge on chromosome and position
 daner_aligned <- 
 daner %>%
-left_join(impute_frq2, by=c('CHR'='CHR', 'BP'='POS'), suffix=c('', '.imp')) %>%
+inner_join(impute_frq2 %>% filter(between(FA1, 0.01, 0.99)),
+          by=c('CHR'='CHR', 'BP'='POS'), suffix=c('', '.imp')) %>%
 # keep rows where alleles match
 filter((A1 == A1.imp & A2 == A2.imp ) | (A1 == A2.imp & A2 == A1.imp)) %>%
+filter(!is.na(OR) & !is.na(SE) & !is.na(P)) %>%
 # select imputed SNP name
 mutate(SNP=SNP.imp, SNP) %>%
-select(-ends_with('.imp'))
+select(-ends_with('.imp'), -FA1) %>%
+arrange(CHR, BP) %>%
+select(CHR, SNP, BP, A1, A2, starts_with('FRQ_A'), starts_with('FRQ_U'), INFO, OR, SE, P, everything())
 
 aligned_gz <- snakemake@output[[1]]
 
@@ -52,14 +56,15 @@ write_tsv(daner_aligned, aligned_gz)
 
 # write log
 N_snps <- nrow(daner)
-mean_or <- mean(daner$OR)
+mean_or <- mean(daner_aligned$OR)
+mean_se <- mean(daner_aligned$SE)
 N_snps_aligned <- nrow(daner_aligned)
 sumstats_cohort <- snakemake@wildcards$cohort
 sumstats_ancestries <- snakemake@wildcards$ancestries
 sumstats_version <- snakemake@wildcards$version
 
 log_table <- data.frame(cohort=sumstats_cohort, ancestries=sumstats_ancestries, version=sumstats_version,
-           N_snps=N_snps, N_snps_aligned=N_snps_aligned, mean_OR=signif(mean_or, 5))
+           N_snps=N_snps, N_snps_aligned=N_snps_aligned, mean_OR=signif(mean_or, 5), mean_SE=signif(mean_se, 5))
 
 write_tsv(log_table, path=log_path)
 
