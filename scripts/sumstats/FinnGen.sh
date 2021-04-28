03b5334d2a10a9a665870b7202ab2b233d8c4fed
Mark James Adams <mark.adams@ed.ac.uk>
2021-04-28 12:58:22 +0200

    Meta: remove conversion-unstable positions (CUPs) before liftOver (PMID 33822888)

--- scripts/sumstats/FinnGen.sh
+++ scripts/sumstats/FinnGen.sh
@@ -11,6 +11,6 @@ daner=$(dirname $daner_gz)/$(basename $daner_gz .gz)
 echo -e "CHR\tSNP\tBP\tA1\tA2\tFRQ_A_23424\tFRQ_U_192220\tINFO\tOR\tSE\tP" > $daner
 
 # convert chromsome X to 23
-zcat $text_gz | tail -n +2 | awk -v OFS='\t' '{if($1 == "X") {print 23, $5, $2, $4, $3, $11, $12, $18, exp($8), $9, $7} else {print $1, $5, $2, $4, $3, $11, $12, $18, exp($8), $9, $7}}' >> $daner
+zcat $text_gz | tail -n +2 | awk -v OFS='\t' '{if($1 == "X") {print "chr23", $5, $2, $4, $3, $11, $12, $18, exp($8), $9, $7} else {print "chr"$1, $5, $2, $4, $3, $11, $12, $18, exp($8), $9, $7}}' >> $daner
 
 gzip -f --verbose $daner 2>> $log
