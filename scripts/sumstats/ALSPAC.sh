fd034ead2cb03043b4dcf85809804e050f9cecf1
Mark James Adams <mark.adams@ed.ac.uk>
2021-06-25 13:50:44 +0200

    Meta: update ALSPAC sumstats

--- scripts/sumstats/ALSPAC.sh
+++ scripts/sumstats/ALSPAC.sh
@@ -6,21 +6,21 @@ log=$3
 
 echo "Converting ALSPAC to daner from $(basename $text_gz)" > $log
 
-    #  1  SNP
-    #  2  CHR
-    #  3  BP
-    #  4  coded_allele
-    #  5  noncoded_allele
-    #  6  strand_genome
-    #  7  BETA (column labelled "OR")
-    #  8  SE
-    #  9  P
-    # 10  n_cases
-    # 11  n_controls
-    # 12  AF_coded_all
-    # 13  AF_coded_cases
-    # 14  AF_coded_controls
-    # 15  info
+#  1	SNP
+#  2	CHR
+#  3	BP
+#  4	effect_allele
+#  5	noneffect_allele
+#  6	strand_genome
+#  7	BETA
+#  8	SE
+#  9	P
+# 10	n_cases
+# 11	n_controls
+# 12	AF_coded_all
+# 13	AF_coded_cases
+# 14	AF_coded_controls
+# 15	info
 
 daner=$(dirname $daner_gz)/$(basename $daner_gz .gz)
 
@@ -32,6 +32,6 @@ echo "Nco: ${Nco}" >> $log
 
 echo -e "CHR\tSNP\tBP\tA1\tA2\tFRQ_A_${Nca}\tFRQ_U_${Nco}\tINFO\tOR\tSE\tP" > $daner
 
-zcat $text_gz | tail -n +2 | awk -v OFS='\t' '{print $2+0, $1, $3, $4, $5, $13, $14, $15, exp($7), $8, $9}' >> $daner
+zcat $text_gz | tail -n +2 | awk -v OFS='\t' '{if($7 != "NA" && $12 >= 0.01 && $12 <= 0.99) {print $2+0, $1, $3, $4, $5, $13, $14, $15, exp($7), $8, $9}}' >> $daner
 
 gzip -f --verbose $daner 2>> $log
