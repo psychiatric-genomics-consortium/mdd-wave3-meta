e246ca48c70fad311f26fd33b71ba4c8610eb008
madams23 <mark.adams@ed.ac.uk>
2021-06-25 11:58:40 +0100

    Meta: Fst checking

--- scripts/meta/filter.R
+++ scripts/meta/filter.R
@@ -28,12 +28,6 @@ qc_mac <- snakemake@params$mac
 qc_secure <- snakemake@params$secure_frq
 qc_diff <- snakemake@params$diff_frq
 
-logging("QC parameters")
-logging(glue("MAF: {qc_maf} (minor allele frequency)"))
-logging(glue("INFO: {qc_info} (imputation score)"))
-logging(glue("MAC: {qc_mac} (minor allele count)"))
-logging(glue("DIFF: {qc_diff} (difference in allele frequency with reference)"))
-
 # get names of FRQ_A and FRQ_U columns
 frq_a_col <- names(select(daner, starts_with('FRQ_A')))
 frq_u_col <- names(select(daner, starts_with('FRQ_U')))
@@ -41,19 +35,45 @@ frq_u_col <- names(select(daner, starts_with('FRQ_U')))
 n_cases <- as.numeric(str_split(frq_a_col, pattern='_')[[1]][3])
 n_controls <- as.numeric(str_split(frq_u_col, pattern='_')[[1]][3])
 
-# Filter
-logging('Applying filters')
-daner_qc <- daner %>%
+# Calculating checks
+
+daner_check <- daner %>%
 # shorter variable names for frequency columns
 mutate(frq_a=.data[[frq_a_col]],
 	   frq_u=.data[[frq_u_col]])%>%
 # remove rows with small minor allele counts and frequencies
 mutate(maf_a=if_else(frq_a <= 0.5, true=frq_a, false=1-frq_a),
 	   maf_u=if_else(frq_u <= 0.5, true=frq_u, false=1-frq_u)) %>%
+# check for extreme allele frequency differences
+mutate(M=(frq_u + FA1.ref)/2,
+	   S=(frq_u^2 + FA1.ref^2)/2,
+	   Fst=(S-M^2)/(M*(1-M)),
+	   D=2*2*(S-M^2)/((2-1)*(1+2*S-2*M)))
+
+median_fst <- median(daner_check$Fst, na.rm=T)
+max_fst <- max(daner_check$Fst, na.rm=T)
+var_fst <- var(daner_check$Fst, na.rm=T)
+logging(glue('Fst: median = {signif(median_fst, 3)}, max = {signif(max_fst, 3)}, var = {signif(var_fst, 3)}'))
+	   
+logging('Permutation test for Fst')
+permute_frq <- daner_check %>%
+transmute(frq1=sample(frq_u, n()),
+          frq2=sample(FA1.ref, n())) %>%
+mutate(M=(frq1 + frq2)/2,
+       S=(frq1^2 + frq2^2)/2,
+       Fst=(S-M^2)/(M*(1-M)),
+       D=2*2*(S-M^2)/((2-1)*(1+2*S-2*M))) 	  
+	   
+qc_fst <- median(permute_frq$Fst, na.rm=T)
+
+
+logging('Applying filters')
+daner_qc <- daner_check %>%
 # Apply QC checks for MAC, MAF, frq different from ref, info
 mutate(QC=case_when(maf_a == 0 | maf_u  == 0 ~ 'MAF',
                     maf_a < qc_maf & maf_u < qc_maf ~ 'MAF',
                     maf_a*n_cases < qc_mac | maf_u*n_controls <= qc_mac ~ 'MAC',
+					Fst > qc_fst ~ 'FST',
 					frq_u - FA1.ref > qc_diff ~ 'DIFF',
 					INFO < qc_info ~ 'INFO',
 					TRUE ~ 'PASS'))
@@ -62,25 +82,27 @@ qc_counts <- table(daner_qc$QC)
 
 snps_maf <- coalesce(qc_counts['MAF'], 0)
 snps_mac <- coalesce(qc_counts['MAC'], 0)
+snps_fst <- coalesce(qc_counts['FST'], 0)
 snps_diff <- coalesce(qc_counts['DIFF'], 0)
 snps_info <- coalesce(qc_counts['INFO'], 0)
 snps_pass <- coalesce(qc_counts['PASS'], 0)
 
-logging(glue('{snps_maf} SNP removed for MAF.'))
-logging(glue('{snps_mac} SNP removed for MAC.'))
-logging(glue('{snps_diff} SNP removed for DIFF.'))
-logging(glue('{snps_info} SNP removed for INFO.'))
+logging(glue('{snps_maf} SNPs removed for MAF < {qc_maf}.'))
+logging(glue('{snps_mac} SNPs removed for MAC < {qc_mac}.'))
+logging(glue('{snps_fst} SNPs removed for Fst > {signif(qc_fst, 4)}'))
+logging(glue('{snps_diff} SNP removed for DIFF > {qc_diff}.'))
+logging(glue('{snps_info} SNP removed for INFO < {qc_info}.'))
 					
 daner_filtered <- daner_qc %>%
 filter(QC == 'PASS') %>%
-select(-ends_with('.ref'), -frq_a, -frq_u, -maf_a, -maf_u, -QC) %>%
+select(-ends_with('.ref'), -frq_a, -frq_u, -maf_a, -maf_u, -QC, -M, -S, -Fst, -D) %>%
 arrange(CHR, BP) %>%
 select(CHR, SNP, BP, A1, A2, starts_with('FRQ_A'), starts_with('FRQ_U'), INFO, OR, SE, P, everything())
 
 
 
 N_snps_kept <- nrow(daner_filtered)
-logging(glue("{N_snps_kept} SNPs after applying QC filters. {N_snps-N_snps_kept} removed."))		
+logging(glue("{N_snps_kept} SNPs remain after applying QC filters. {N_snps-N_snps_kept} removed."))		
 
 filtered_gz <- snakemake@output$daner
 
@@ -100,6 +122,8 @@ sumstats_release <- snakemake@wildcards$release
 
 qc_table <- data.frame(cohort=sumstats_cohort, ancestries=sumstats_ancestries, release=sumstats_release,
 		                N_cases=n_cases, N_controls=n_controls,
+						median_fst=signif(median_fst, 3), max_fst=signif(max_fst, 3),
+						var_fst=signif(var_fst, 3),
 						snps_kept=N_snps_kept,
 						median_or=round(median_or, 4), max_OR=round(max_or, 4),
 						median_SE=round(median_se, 4), max_SE=round(max_se, 4))
