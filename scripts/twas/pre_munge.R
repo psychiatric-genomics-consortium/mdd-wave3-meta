c56d89edb80121ccea2bf371f9efb471fbaee0bd
Oliver Pain <oliver.pain@kcl.ac.uk>
2021-10-07 16:24:30 +0100

    Use Neff rather than Neff_half. Added scripts for running eQTLGen SMR and significance threshold investigation

---
+++ scripts/twas/pre_munge.R
@@ -0,0 +1,10 @@
+#!/usr/bin/Rscript
+args = commandArgs(trailingOnly=TRUE)
+                   
+library(data.table)
+ss<-fread('results/distribution/daner_pgc_mdd_full_eur_hg19_v3.49.24.05.rp.gz')
+
+ss<-ss[,c(1:5,8:11,19),with=F]
+ss$N<-ss$Neff_half*2
+
+fwrite(ss, 'results/twas/munged_gwas/daner_pgc_mdd_full_eur_hg19_v3.49.24.05.rp_premunged.gz', sep=' ', na='NA', quote=F)
