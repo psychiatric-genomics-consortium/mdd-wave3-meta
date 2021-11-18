76927cc31c720eed2e6da8153e4ed85af7a73b4a
Mark James Adams <mark.adams@ed.ac.uk>
2021-11-18 16:12:20 +0100

    Meta: add clumped results file to repository

---
+++ scripts/meta/rp_clump.R
@@ -0,0 +1,22 @@
+library(readr)
+library(dplyr)
+
+auto_clump <- read_table(snakemake@input$auto)
+allo_clump <- read_table(snakemake@input$allo)
+
+# get names of FRQ_A and FRQ_U columns
+frq_a_auto_col <- names(select(auto_clump, starts_with('FRQ_A')))
+frq_u_auto_col <- names(select(auto_clump, starts_with('FRQ_U')))
+frq_a_allo_col <- names(select(allo_clump, starts_with('FRQ_A')))
+frq_u_allo_col <- names(select(allo_clump, starts_with('FRQ_U')))
+
+clump <- bind_rows(
+auto_clump,
+allo_clump %>% rename(!!frq_a_auto_col:=frq_a_allo_col,
+                      !!frq_u_auto_col:=frq_u_allo_col) %>%
+                mutate(ngt=if_else(ngt=='-', true=NA_real_, false=as.numeric(ngt)))
+) %>%
+filter(P <= 5e-8) %>%
+arrange(P)
+
+write_tsv(clump, snakemake@output[[1]])
