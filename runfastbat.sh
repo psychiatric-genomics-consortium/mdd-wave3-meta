5f6d3aaca4240bd9dd71b57d1d52a6df466e10cc
davemhoward <David.Howard@kcl.ac.uk>
2022-04-14 10:42:11 +0100

    Update runfastbat.sh
    Update to run across all genes (N = 58844) as well as the protein coding genes (N = 19878)

--- runfastbat.sh
+++ runfastbat.sh
@@ -1,7 +1,16 @@
-## Run fastBAT within GCTA using qc'd summary stats and geneMatrix gene list
+## Run fastBAT within GCTA using qc'd summary stats and geneMatrix protein coding gene list (N = 19878)
 
 ./gcta_v1.94.0Beta_linux_kernel_3_x86_64/gcta_v1.94.0Beta_linux_kernel_3_x86_64_static \
 --bfile all_phase3.eur \
 --fastBAT mdd_fastbat_neff_maf01_info80.txt \
 --fastBAT-gene-list geneMatrixForFastBAT.txt \
 --out mdd_fastbat_geneMatrix
+
+
+## Run fastBAT within GCTA using qc'd summary stats and all geneMatrix gene list (N = 58844)
+
+./gcta_v1.94.0Beta_linux_kernel_3_x86_64/gcta_v1.94.0Beta_linux_kernel_3_x86_64_static \
+--bfile all_phase3.eur \
+--fastBAT mdd_fastbat_neff_maf01_info80.txt \
+--fastBAT-gene-list AllgeneMatrixForFastBAT.txt \
+--out mdd_fastbat_AllgeneMatrix
